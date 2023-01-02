a1e54ee396ca6c8e2a9a3030368ac1831d8bc2f1
bc20git01 <root@localhost.localdomain>
2023-01-02 15:59:48 -0500

    update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,9 @@
 # echo $PASS >> /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 # Pass as hidden files
-scp -i /opt/id_rsa deploy/publish root@192.168.1.78:~/publish
-scp -i /opt/id_rsa /tmp/.auth root@192.168.1.78:~/.auth
-ssh -i /opt/id_rsa root@192.168.1.78 '~/publish'
+cd /home/jenkinssvc/pipe-app-deploy/jenkins-github
+
+scp -i /opt/key ./deploy/publish 192.168.1.78:~/publish
+scp -i /opt/key /tmp/.auth 192.168.1.78:~/.auth
+ssh -i /opt/key 192.168.1.78 '~/publish'
 
