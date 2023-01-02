45a56cd0e87293a877bd12e3ae71bb393d18cc87
bc20git01 <root@localhost.localdomain>
2023-01-02 15:42:57 -0500

    change

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@
 # echo $PASS >> /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 # Pass as hidden files
-scp -i /opt/key deploy/publish root@192.168.1.78:~/publish
-scp -i /opt/key /tmp/.auth root@192.168.1.78:~/.auth
-ssh -i /opt/key root@192.168.1.78 '~/publish'
+scp -i /opt/id_rsa deploy/publish root@192.168.1.78:~/publish
+scp -i /opt/id_rsa /tmp/.auth root@192.168.1.78:~/.auth
+ssh -i /opt/id_rsa root@192.168.1.78 '~/publish'
 
