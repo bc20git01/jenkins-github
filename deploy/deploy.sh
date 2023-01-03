c78e0befacec1b60de8a65b2543ce7247edc2302
bc20git01 <root@localhost.localdomain>
2023-01-02 19:53:01 -0500

    new

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,10 @@ echo $BUILD_TAG >> /tmp/.auth
 # Pass as hidden files
 
 WORKSPACE=/home/jenkinssvc/pipe-app-deploy/jenkins-github
-ssh -o StrictHostKeyChecking=no -i /opt/key root@192.168.1.78
+ssh -o StrictHostKeyChecking=no -i /opt/key jenkins@192.168.1.78
+
+echo "..... Start SCP Copy ....."
+
 scp -o StrictHostKeyChecking=no -i /opt/key $WORKSPACE/deploy/publish 192.168.1.78:~/publish
 scp -o StrictHostKeyChecking=no -i /opt/key /tmp/.auth 192.168.1.78:~/.auth
 ssh -o StrictHostKeyChecking=no -i /opt/key 192.168.1.78 '~/publish'
